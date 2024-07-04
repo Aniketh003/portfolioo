9636995b4a2788c550cffc489a07498e7aa5274f
Aniketh003 <21wj1a6912@gniindia.org>
2024-07-04 15:43:49 +0530

    removed works animation

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -2,40 +2,14 @@ import React, { useEffect, useRef } from 'react';
 import styles from './works.module.css';
 import { projects } from './projects';
 import Project from './Project';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-
-gsap.registerPlugin(ScrollTrigger);
 
 const Works = () => {
-    const projectsRef = useRef([]);
-
-    useEffect(() => {
-        projectsRef.current.forEach((project, index) => {
-            const direction = index % 2 === 0 ? 'fromLeft' : 'fromRight';
-            const fromVars = direction === 'fromLeft' ? { x: -200, opacity: 0 } : { x: 200, opacity: 0 };
-            const toVars = { x: 0, opacity: 1, duration: 1, ease: 'power2.out' };
-
-            gsap.fromTo(project, fromVars, {
-                ...toVars,
-                scrollTrigger: {
-                    trigger: project,
-                    start: 'top center',
-                    end: 'bottom center',
-                    
-                    toggleActions: 'play reverse play reverse',
-                }
-            });
-        });
-    }, []);
-
     return (
         <div className={styles.works}>
             <h1>WORKS</h1>
             <div className={styles.projects}>
-                {projects.map((project, index) => (
+                {projects.map((project) => (
                     <div
-                        ref={el => projectsRef.current[index] = el}
                         key={project.source}
                         className={styles.project_card}
                     >
